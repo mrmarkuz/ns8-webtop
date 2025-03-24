00fcd0df027b26144d179f16d61fdda8c4117a6a
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2025-03-24 10:42:45 +0100

    feat(pecbridge): update to version 5.4.5 (#110)
    Add new env variables:
    
    - PECBRIDGE_FROM_ADDRESS
    - PECBRIDGE_NOTIFY_OWNER
    
    NethServer/dev#7352

--- webapp/usr/share/pecbridge/bin/run.sh
+++ webapp/usr/share/pecbridge/bin/run.sh
@@ -14,8 +14,22 @@ cd "$PBHOME"
 CLASSPATH="lib/*:classes"
 export PATH CLASSPATH
 
+if [ -n $PECBRIDGE_FROM_ADDRESS ]; then
+    from_address="fromAddress=\"$PECBRIDGE_FROM_ADDRESS\""
+else
+    from_address=""
+fi
+
+if [ -n $PECBRIDGE_NOTIFY_OWNER ]; then
+    notify_owner="notifyOwner=\"$PECBRIDGE_NOTIFY_OWNER\""
+else
+    notify_owner=""
+fi
+
 # Expand the Pecbridge configuration file template:
 sed -e "s/WEBAPP_API_TOKEN/${WEBAPP_API_TOKEN:?}/" \
+    -e "s/PECBRIDGE_FROM_ADDRESS/$from_address/" \
+    -e "s/PECBRIDGE_NOTIFY_OWNER/$notify_owner/" \
     ${PECBRIDGE_ADMIN_MAIL:+-e "1 s/sonicle-pec-bridge /sonicle-pec-bridge adminMail=\"${PECBRIDGE_ADMIN_MAIL}\" /"} \
     etc/config.xml.template > etc/config.xml
 
